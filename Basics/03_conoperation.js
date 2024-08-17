be5063299af3428ed8d73ccdfd5b7efe727f9f50
Kausharalam7 <Kausharalam5577@gmail.com>
2024-08-17 16:51:23 +0000

    conversion operation

---
+++ Basics/03_conoperation.js
@@ -0,0 +1,23 @@
+// let score="33axd"
+
+// console.log(typeof(score));
+// console.log(typeof score);
+
+// let valueInNumber=Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber)
+
+let value=3;
+let negValue=-value;
+console.log(negValue);
+
+let str1="kaushar"
+let str2="alam"
+console.log(str1+str2);
+console.log(+true)
+console.log(true)
+console.log(+"")
+
+let gameCounter=100;
+++gameCounter;
+console.log(gameCounter)

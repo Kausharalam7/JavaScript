5dbc1b32bda5f7afae80bfb981ae6d0b4c2db42c
Kausharalam7 <Kausharalam5577@gmail.com>
2024-08-17 16:23:36 +0000

    datatypes

---
+++ Basics/02_datatypes.js
@@ -0,0 +1,12 @@
+"use strict"; // treat all Js code as newer version
+// alert(3+4)  we are using nodejs, not browser
+console.log(3+3);
+console.log("kaushar")
+let name="amar"
+let age=24;
+let isLoggedIn =false;
+
+
+console.log(typeof undefined); //undefined
+console.log(typeof null); // object
+

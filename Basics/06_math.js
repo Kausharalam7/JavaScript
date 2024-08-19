b7b401938a46f14ae29b57d09e421aa0e1bef3ee
Kausharalam7 <Kausharalam5577@gmail.com>
2024-08-19 16:00:38 +0000

    add

--- Basics/06_math.js
+++ Basics/06_math.js
@@ -1,7 +1,22 @@
 const score=400;
 
-const balance =new Number(100)
-console.log(balance)
-console.log(balance.toString().length);
-const othernum=231.1234;
-console.log(othernum.toPrecision(4));
+// const balance =new Number(100)
+// console.log(balance)
+// console.log(balance.toString().length);
+// const othernum=231.1234;
+// console.log(othernum.toPrecision(4));
+
+const hundreds = 100000;
+// console.log(hundreds.toLocaleString('en-IN'));
+// console.log(Math)
+// console.log(Math.abs(-45))
+// console.log(Math.abs(34));
+
+console.log(Math.random());
+console.log(Math.random()*10 +1);
+console.log(Math.floor(Math.random()*10) + 1);
+
+const min=10;
+const max=20;
+
+console.log(Math.floor(Math.random() * (max-min +1) +min))
